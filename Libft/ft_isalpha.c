f9c0909ba1249799412ae10e3db83f2e939b3370
jkeum <jon9eun@gmail.com>
2020-10-11 21:44:42 +0900

    ft_isalpha.c modified

--- Libft/ft_isalpha.c
+++ Libft/ft_isalpha.c
@@ -6,13 +6,27 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 16:37:30 by jkeum             #+#    #+#             */
-/*   Updated: 2020/09/30 16:37:54 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/10/07 21:13:03 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalpha(int c)
+static int	ft_isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
+
+static int	ft_islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+
+int			ft_isalpha(int c)
 {
 	return (ft_isupper(c) || ft_islower(c));
 }
