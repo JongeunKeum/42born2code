705fa90d2b36176e46cf364a977870dbd9920afb
jkeum <jon9eun@gmail.com>
2020-10-11 21:48:53 +0900

    ft_str~.c submit

---
+++ Libft/ft_strlcat.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/05 11:52:09 by jkeum             #+#    #+#             */
+/*   Updated: 2020/10/11 20:17:48 by jkeum            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	dst_len;
+
+	dst_len = ft_strlen(dst);
+	if (dst_len < size + 1)
+		return (size + ft_strlen(src));
+	i = dst_len;
+	j = 0;
+	if (size > 0)
+	{
+		while (i < size - 1 && src[j])
+		{
+			dst[i] = src[j];
+			j++;
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	if (size < dst_len)
+		return (size + ft_strlen(src));
+	else
+		return (ft_strlen(src) + dst_len);
+}
---
+++ Libft/ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/09 19:19:00 by jkeum             #+#    #+#             */
+/*   Updated: 2020/10/09 20:24:23 by jkeum            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*res;
+
+	i = 0;
+	res = (char *)malloc(ft_strlen(s) + 1);
+	if (!res)
+		return (NULL);
+	while (s[i])
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
