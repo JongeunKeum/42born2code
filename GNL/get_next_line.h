bfc88cccbbe84a06f2a00902d4ef5dde92dc17da
jkeum <jon9eun@gmail.com>
2020-10-23 15:17:37 +0900

    get_next_line submit

---
+++ GNL/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/22 16:40:03 by jkeum             #+#    #+#             */
+/*   Updated: 2020/10/23 14:52:44 by jkeum            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
+int		ft_strlen(char *str);
+char	*ft_strjoin_gnl(char *save, char *buff);
+
+#endif
