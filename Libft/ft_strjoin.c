83547a3ccf1c13cce4a66c4eebf6d9039d41dd5c
jkeum <jon9eun@gmail.com>
2020-10-24 17:03:13 +0900

    ft_strjoin modified

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 06:05:01 by jkeum             #+#    #+#             */
-/*   Updated: 2020/10/20 21:40:50 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/10/24 17:01:28 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
+	size_t	i;
+	size_t	j;
 	size_t	s1_len;
 	size_t	s2_len;
 	char	*res;
@@ -24,8 +26,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	s2_len = ft_strlen(s2);
 	if (!(res = (char *)malloc(s1_len + s2_len + 1)))
 		return (NULL);
-	ft_memcpy(res, s1, s1_len);
-	ft_memcpy(res + s1_len, s2, s2_len);
-	res[s1_len + s2_len] = '\0';
+	i = 0;
+	while (i < s1_len)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < s2_len)
+	{
+		res[i + j] = s2[j];
+		j++;
+	}
+	res[i + j] = '\0';
 	return (res);
 }
