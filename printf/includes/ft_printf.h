b6d3b5d4dc70ff2cfde22d924d79cbd55483d416
jkeum <jon9eun@gmail.com>
2020-11-07 21:47:56 +0900

    ft_printf.h modified

--- printf/includes/ft_printf.h
+++ printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 15:38:45 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/06 16:48:36 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/07 21:25:51 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include <limits.h>
 # include "../libft/libft.h"
 
 typedef struct	s_obj
@@ -30,9 +31,15 @@ typedef struct	s_obj
 	int		len;
 	int		idx;
 	int		neg;
+	int		dot;
+	int		return_value;
 }				t_obj;
 
 int		print_int(va_list args, t_obj *obj);
 int		check_format(const char *str, va_list args, t_obj *obj);
+void	check_flag(const char *str, t_obj *obj);
+void	check_precision(const char *str, t_obj *obj, va_list args);
+void	check_width(const char *str, t_obj *obj, va_list args);
+char	*ft_lltoa(long long n);
 
 #endif
