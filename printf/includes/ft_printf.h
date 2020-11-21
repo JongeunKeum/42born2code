9c7d99d37fc129b41c9550932b23da0c429998f1
jkeum <jon9eun@gmail.com>
2020-11-21 18:16:35 +0900

    ft_printf.h modified (add files)

--- printf/includes/ft_printf.h
+++ printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 15:38:45 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/14 16:47:27 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/21 18:06:30 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ typedef struct	s_obj
 	int		idx;
 	int		neg;
 	int		dot;
+	int		length;
 	int		return_value;
 }				t_obj;
 
@@ -42,15 +43,23 @@ int			print_str(va_list args, t_obj *obj);
 int			print_unsigned_int(va_list args, t_obj *obj);
 int			print_hex(va_list args, t_obj *obj);
 int			print_oct(va_list args, t_obj *obj);
+int			print_address(va_list args, t_obj *obj);
+int			print_percent(t_obj *obj);
+int			print_num_ch(va_list args, t_obj *obj);
 void		check_width(const char *str, t_obj *obj, va_list args);
 void		check_precision(const char *str, t_obj *obj, va_list args);
 void		check_flag(const char *str, t_obj *obj);
 int			check_type(va_list args, t_obj *obj);
 int			check_format(const char *str, va_list args, t_obj *obj);
+void		check_length(const char *str, t_obj *obj);
 char		*fill_precision_nbr(t_obj *obj, char *prev);
 char		*fill_width(t_obj *obj, char *prev);
 char		*fill_width_null(t_obj *obj, char c);
 char		*ft_lltoa(long long n);
+char		*ft_ptoa(long long n);
 char		*ft_convert_base(char *nbr, char *base_from, char *base_to);
+char		*ft_strjoin_free1(char *s1, char *s2);
+char		*ft_strjoin_free2(char *s1, char *s2);
+char		*ft_strjoin_free3(char *s1, char *s2);
 
 #endif
