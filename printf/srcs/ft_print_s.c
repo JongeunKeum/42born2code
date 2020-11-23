340a16c39c17a7ef4730a52c9d73afa6bad67d1c
jkeum <jon9eun@gmail.com>
2020-11-23 15:31:00 +0900

    ft_print_s.c modified (completed)

--- printf/srcs/ft_print_s.c
+++ printf/srcs/ft_print_s.c
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 14:58:56 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/16 19:33:36 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/22 14:16:24 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,21 @@
 int		print_str(va_list args, t_obj *obj)
 {
 	char	*s;
-	char	*res;
+	char	*null;
 
 	s = va_arg(args, char *);
+	null = ft_strdup("(null)");
 	if (!s)
-		s = ft_strdup("(null)");
-	else
-		res = ft_strdup("");
+		s = null;
 	obj->len = ft_strlen(s);
 	if (obj->dot && obj->precision < obj->len)
-		res = ft_substr(s, 0, obj->precision);
+		obj->res = ft_substr(s, 0, obj->precision);
 	else
-		res = ft_strjoin(res, s);
-	if (obj->width > (int)ft_strlen(res))
-		res = fill_width(obj, res);
-	ft_putstr_fd(res, 1);
-	obj->return_value += ft_strlen(res);
-	free(res);
+		obj->res = ft_strjoin(obj->res, s);
+	if (obj->width > (int)ft_strlen(obj->res))
+		fill_width(obj);
+	ft_putstr_fd(obj->res, 1);
+	obj->return_value += ft_strlen(obj->res);
+	free(null);
 	return (1);
 }
