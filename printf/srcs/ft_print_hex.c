8a128d3baf98a33c84a2e8564f26b448acf64b94
Jongeun Keum <jon9eun@gmail.com>
2020-11-23 16:23:41 +0900

    ft_print_hex.c norminette

--- printf/srcs/ft_print_hex.c
+++ printf/srcs/ft_print_hex.c
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 20:09:09 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/23 13:24:19 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/23 16:19:43 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,16 @@ unsigned long long	get_num_x(va_list args, t_obj *obj)
 	return (n);
 }
 
-void	is_type_X(t_obj *obj)
+void				is_type_upper(t_obj *obj)
 {
 	int	i;
-	
+
 	i = -1;
 	while (obj->res[++i])
 		obj->res[i] = ft_toupper(obj->res[i]);
 }
 
-void	process_width_x(t_obj *obj)
+void				process_width_x(t_obj *obj)
 {
 	if (obj->width > (int)ft_strlen(obj->res) + obj->prefix)
 		fill_width(obj);
@@ -49,7 +49,7 @@ void	process_width_x(t_obj *obj)
 	}
 }
 
-int		print_hex(va_list args, t_obj *obj)
+int					print_hex(va_list args, t_obj *obj)
 {
 	unsigned long long	n;
 	char				*nbr;
@@ -64,12 +64,12 @@ int		print_hex(va_list args, t_obj *obj)
 	if (obj->precision > obj->len)
 		fill_precision_nbr(obj);
 	if (n != 0 || (!obj->dot || obj->precision))
-		obj->res = ft_strjoin(obj->res, nbr); 
+		obj->res = ft_strjoin(obj->res, nbr);
 	if (n == 0)
 		obj->prefix = 0;
 	process_width_x(obj);
-	if (obj->type == 'X') 
-		is_type_X(obj);
+	if (obj->type == 'X')
+		is_type_upper(obj);
 	ft_putstr_fd(obj->res, 1);
 	obj->return_value += ft_strlen(obj->res);
 	free(nbr);
