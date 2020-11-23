99711abe5ea767a98b5814d98aab093eb9171a39
jkeum <jon9eun@gmail.com>
2020-11-23 15:31:55 +0900

    ft_print_u.c modified (completed)

--- printf/srcs/ft_print_u.c
+++ printf/srcs/ft_print_u.c
@@ -6,32 +6,42 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/12 15:13:22 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/22 14:21:25 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/23 13:24:30 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int		print_unsigned_int(va_list args, t_obj *obj)
+unsigned long long	get_num_u(va_list args, t_obj *obj)
 {
 	unsigned long long	n;
-	char				*nbr;
-
+	
 	if (obj->length == 3)
 		n = va_arg(args, unsigned long);
 	else if (obj->length == 4)
 		n = va_arg(args, unsigned long long);
+	else if (obj->length == 2)
+		n = (unsigned char)va_arg(args, int);
 	else if (obj->length == 1)
 		n = (unsigned short)va_arg(args, int);
 	else
 		n = va_arg(args, unsigned int);
 	if (obj->sign)
 		obj->space = 0;
+	return (n);
+}
+
+int		print_unsigned_int(va_list args, t_obj *obj)
+{
+	unsigned long long	n;
+	char				*nbr;
+
+	n = get_num_u(args, obj);
 	nbr = ft_lltoa(n);
 	obj->len = ft_strlen(nbr);
 	if (obj->precision > obj->len)
 		fill_precision_nbr(obj);
-	if (!obj->dot || obj->precision)
+	if (n != 0 || (!obj->dot || obj->precision))
 		obj->res = ft_strjoin(obj->res, nbr + obj->neg);
 	if (obj->width > (int)ft_strlen(obj->res))
 		fill_width(obj);
