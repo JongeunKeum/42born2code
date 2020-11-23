f4c49a8a4f26b235139b5c4378814ac7198b1f77
jkeum <jon9eun@gmail.com>
2020-11-23 15:30:35 +0900

    ft_print_p.c modified (completed)

--- printf/srcs/ft_print_p.c
+++ printf/srcs/ft_print_p.c
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 15:51:46 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/18 19:27:19 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/22 15:08:56 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 int	print_percent(t_obj *obj)
 {
-	char	*res;
+	char	*per;
 
-	res = ft_strdup("%");
-	obj->len = ft_strlen(res);
+	per = ft_strdup("%");
+	obj->res = ft_strjoin(per, obj->res);
+	obj->len = ft_strlen(obj->res);
 	if (obj->width > obj->len)
-		res = fill_width(obj, res);
-	ft_putstr_fd(res, 1);
-	obj->return_value += ft_strlen(res);
-	free(res);
+		fill_width(obj);
+	ft_putstr_fd(obj->res, 1);
+	obj->return_value += ft_strlen(obj->res);
+	free(per);
 	return (1);
 }
 
@@ -30,27 +31,24 @@ int	print_address(va_list args, t_obj *obj)
 {
 	unsigned long long	n;
 	char				*adr;
-	char				*res;
 
 	n = (unsigned long long)va_arg(args, void *);
 	adr = ft_ptoa(n);
 	obj->len = ft_strlen(adr);
 	obj->prefix = 2;
-	res = ft_strdup("");
 	if (obj->precision > obj->len)
-		res = fill_precision_nbr(obj, res);
+		fill_precision_nbr(obj);
 	if ((!obj->dot || obj->precision) || n != 0)
-		res = ft_strjoin(res, adr);
-	if (obj->width > (int)ft_strlen(res) + obj->prefix)
-		res = fill_width(obj, res);
+		obj->res = ft_strjoin(obj->res, adr);
+	if (obj->width > (int)ft_strlen(obj->res) + obj->prefix)
+		fill_width(obj);
 	else
 	{
 		if (obj->prefix)
-			res = ft_strjoin("0x", res);
+			obj->res = ft_strjoin("0x", obj->res);
 	}
-	ft_putstr_fd(res, 1);
-	obj->return_value += ft_strlen(res);
+	ft_putstr_fd(obj->res, 1);
+	obj->return_value += ft_strlen(obj->res);
 	free(adr);
-	free(res);
 	return (1);
 }
