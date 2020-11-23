c01a94622885aad12d3b42b2fd97e07eb4420e4e
jkeum <jon9eun@gmail.com>
2020-11-23 19:05:07 +0900

    ft_strjoin.c norminette

--- printf/libft/ft_strjoin.c
+++ printf/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/09 06:05:01 by jkeum             #+#    #+#             */
-/*   Updated: 2020/10/24 17:01:28 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/23 19:04:46 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s2_len;
 	char	*res;
 
-	if (!s1 || !s2)
-		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	if (!(res = (char *)malloc(s1_len + s2_len + 1)))
