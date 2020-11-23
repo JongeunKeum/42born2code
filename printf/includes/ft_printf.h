7e7be97d45a26efc0bc8eed6e803e8cc538f5158
jkeum <jon9eun@gmail.com>
2020-11-23 15:35:37 +0900

    ft_printf.h modified

--- printf/includes/ft_printf.h
+++ printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 15:38:45 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/22 13:37:36 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/23 13:10:45 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,29 +38,36 @@ typedef struct	s_obj
 	char	*res;
 }				t_obj;
 
-int			print_int(va_list args, t_obj *obj);
-int			print_char(va_list args, t_obj *obj);
-int			print_str(va_list args, t_obj *obj);
-int			print_unsigned_int(va_list args, t_obj *obj);
-int			print_hex(va_list args, t_obj *obj);
-int			print_oct(va_list args, t_obj *obj);
-int			print_address(va_list args, t_obj *obj);
-int			print_percent(t_obj *obj);
-int			print_num_ch(va_list args, t_obj *obj);
-void		check_width(const char *str, t_obj *obj, va_list args);
-void		check_precision(const char *str, t_obj *obj, va_list args);
-void		check_flag(const char *str, t_obj *obj);
-int			check_type(va_list args, t_obj *obj);
-int			check_format(const char *str, va_list args, t_obj *obj);
-void		check_length(const char *str, t_obj *obj);
-void		fill_precision_nbr(t_obj *obj);
-void		fill_width(t_obj *obj);
-void		fill_width_null(t_obj *obj, char c);
-char		*ft_lltoa(long long n);
-char		*ft_ptoa(long long n);
-char		*ft_convert_base(char *nbr, char *base_from, char *base_to);
-char		*ft_strjoin_free1(char *s1, char *s2);
-char		*ft_strjoin_free2(char *s1, char *s2);
-char		*ft_strjoin_free3(char *s1, char *s2);
+int					print_int(va_list args, t_obj *obj);
+int					print_char(va_list args, t_obj *obj);
+int					print_str(va_list args, t_obj *obj);
+int					print_unsigned_int(va_list args, t_obj *obj);
+int					print_hex(va_list args, t_obj *obj);
+int					print_oct(va_list args, t_obj *obj);
+int					print_address(va_list args, t_obj *obj);
+int					print_percent(t_obj *obj);
+int					print_num_ch(va_list args, t_obj *obj);
+long long			get_num_d(va_list args, t_obj *obj);
+unsigned long long	get_num_u(va_list args, t_obj *obj);
+unsigned long long	get_num_o(va_list args, t_obj *obj);
+unsigned long long	get_num_x(va_list args, t_obj *obj);
+void				check_width(const char *str, t_obj *obj, va_list args);
+void				check_precision(const char *str, t_obj *obj, va_list args);
+void				check_flag(const char *str, t_obj *obj);
+int					check_type(va_list args, t_obj *obj);
+int					check_format(const char *str, va_list args, t_obj *obj);
+void				check_length(const char *str, t_obj *obj);
+void				fill_precision_nbr(t_obj *obj);
+void				fill_width(t_obj *obj);
+void				fill_width_null(t_obj *obj, char c);
+void				is_left(t_obj *obj, int len, int flag);
+void				is_zeroflag(t_obj *obj, int len, int flag);
+void				no_left_zeroflag(t_obj *obj, int len, int flag);
+void				is_not_null_c(t_obj *obj, char c);
+void				is_type_X(t_obj *obj);
+void				process_width_x(t_obj *obj);
+char				*ft_lltoa(long long n);
+char				*ft_ptoa(long long n);
+char				*ft_convert_base(char *nbr, char *base_from, char *base_to);
 
 #endif
