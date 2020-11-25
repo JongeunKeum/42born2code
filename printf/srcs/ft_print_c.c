4f10c3fc57eb7cf085c375ba7f992ea05a435e35
jkeum <jon9eun@gmail.com>
2020-11-25 21:54:54 +0900

    ft_print_c.c modified

--- printf/srcs/ft_print_c.c
+++ printf/srcs/ft_print_c.c
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 17:10:09 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/23 13:24:12 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/25 21:44:23 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@ void	fill_width_null(t_obj *obj, char c)
 	char	*wid;
 
 	len = obj->width - 1;
-	wid = (char *)calloc(len + 1, 1);
+	if (!(wid = (char *)ft_calloc(len + 1, 1)))
+		return ;
 	wid = ft_memset(wid, ' ', len);
-	obj->res = ft_strjoin(wid, obj->res);
+	if (!(obj->res = ft_strjoin(wid, obj->res)))
+	{
+		free(wid);
+		return ;
+	}
 	if (obj->left)
 	{
 		ft_putchar_fd(c, 1);
@@ -38,9 +43,14 @@ void	is_not_null_c(t_obj *obj, char c)
 {
 	char	*ch;
 
-	ch = (char *)calloc(2, 1);
+	if (!(ch = (char *)ft_calloc(2, 1)))
+		return ;
 	ch[0] = c;
-	obj->res = ft_strjoin(obj->res, ch);
+	if (!(obj->res = ft_strjoin(obj->res, ch)))
+	{
+		free(ch);
+		return ;
+	}
 	if (obj->width > (int)ft_strlen(obj->res))
 		fill_width(obj);
 	ft_putstr_fd(obj->res, 1);
