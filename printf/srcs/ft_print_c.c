2955ede2ef21069e5399c27b9a2b4d235b082694
jkeum <jon9eun@gmail.com>
2020-11-26 21:26:15 +0900

    ft_print_c.c modified (exception handling, norminette)

--- printf/srcs/ft_print_c.c
+++ printf/srcs/ft_print_c.c
@@ -6,25 +6,25 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 17:10:09 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/25 21:44:23 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/26 17:46:31 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-void	fill_width_null(t_obj *obj, char c)
+int	fill_width_null(t_obj *obj, char c)
 {
 	int		len;
 	char	*wid;
 
 	len = obj->width - 1;
 	if (!(wid = (char *)ft_calloc(len + 1, 1)))
-		return ;
+		return (0);
 	wid = ft_memset(wid, ' ', len);
 	if (!(obj->res = ft_strjoin(wid, obj->res)))
 	{
 		free(wid);
-		return ;
+		return (0);
 	}
 	if (obj->left)
 	{
@@ -37,39 +37,48 @@ void	fill_width_null(t_obj *obj, char c)
 		ft_putchar_fd(c, 1);
 	}
 	free(wid);
+	return (1);
 }
 
-void	is_not_null_c(t_obj *obj, char c)
+int	is_not_null_c(t_obj *obj, char c)
 {
 	char	*ch;
 
 	if (!(ch = (char *)ft_calloc(2, 1)))
-		return ;
+		return (0);
 	ch[0] = c;
 	if (!(obj->res = ft_strjoin(obj->res, ch)))
 	{
 		free(ch);
-		return ;
+		return (0);
 	}
+	free(ch);
 	if (obj->width > (int)ft_strlen(obj->res))
-		fill_width(obj);
+	{
+		if (!(fill_width(obj)))
+			return (0);
+	}
 	ft_putstr_fd(obj->res, 1);
 	obj->return_value += ft_strlen(obj->res);
-	free(ch);
+	return (1);
 }
 
-int		print_char(va_list args, t_obj *obj)
+int	print_char(va_list args, t_obj *obj)
 {
 	char	c;
 
 	c = (char)va_arg(args, int);
 	if (c != '\0')
-		is_not_null_c(obj, c);
+	{
+		if (!(is_not_null_c(obj, c)))
+			return (0);
+	}
 	else
 	{
 		if (obj->width > 1)
 		{
-			fill_width_null(obj, c);
+			if (!(fill_width_null(obj, c)))
+				return (0);
 			obj->return_value += ft_strlen(obj->res) + 1;
 		}
 		else
