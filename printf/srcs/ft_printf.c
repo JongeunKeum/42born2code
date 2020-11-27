2be6e0da7063d6080842b397cd13e2f75d94981c
jkeum <jon9eun@gmail.com>
2020-11-27 17:29:02 +0900

    ft_printf.c modified (memory leak)

--- printf/srcs/ft_printf.c
+++ printf/srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 17:25:31 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/26 17:44:06 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/27 17:24:14 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,8 @@ t_obj	*init_objs(void)
 
 	if (!(obj = (t_obj *)malloc(sizeof(t_obj))))
 		return (NULL);
-	obj->left = 0;
-	obj->zero = 0;
-	obj->sign = 0;
-	obj->space = 0;
-	obj->prefix = 0;
-	obj->width = 0;
-	obj->precision = 0;
-	obj->type = 0;
-	obj->len = 0;
 	obj->idx = 0;
-	obj->neg = 0;
-	obj->dot = 0;
-	obj->length = 0;
 	obj->return_value = 0;
-	if (!(obj->res = (char *)ft_calloc(1, 1)))
-		return (NULL);
 	return (obj);
 }
 
@@ -51,8 +37,6 @@ void	set_objs(t_obj *obj)
 	obj->neg = 0;
 	obj->dot = 0;
 	obj->length = 0;
-	if (!(obj->res = (char *)ft_calloc(1, 1)))
-		return ;
 }
 
 int		ft_printf(const char *str, ...)
@@ -60,6 +44,7 @@ int		ft_printf(const char *str, ...)
 	va_list	args;
 	t_obj	*obj;
 	int		result;
+	char	*temp;
 
 	va_start(args, str);
 	if (!(obj = init_objs()))
@@ -68,14 +53,16 @@ int		ft_printf(const char *str, ...)
 	{
 		if (str[obj->idx] == '%')
 		{
-			if (!check_format(str, args, obj))
+			set_objs(obj);
+			temp = ft_strdup("");
+			obj->res = temp;
+			if (!(check_format(str, args, obj)))
 			{
 				free(obj->res);
 				free(obj);
 				return (-1);
 			}
 			free(obj->res);
-			set_objs(obj);
 		}
 		else
 		{
@@ -85,7 +72,6 @@ int		ft_printf(const char *str, ...)
 		obj->idx++;
 	}
 	va_end(args);
-	free(obj->res);
 	result = obj->return_value;
 	free(obj);
 	return (result);
