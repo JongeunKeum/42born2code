0d6153c340c0eced8a1df0b1038a2bbcd12d4674
jkeum <jon9eun@gmail.com>
2020-11-29 17:24:42 +0900

    norminette, memory leak solved

--- printf/srcs/ft_convert_base.c
+++ printf/srcs/ft_convert_base.c
@@ -6,13 +6,13 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/18 15:40:37 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/29 15:34:44 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/11/29 16:56:11 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-unsigned long long	ft_atoull(char *str, int len, int *match)
+static unsigned long long	ft_atoull(char *str, int len, int *match)
 {
 	int					i;
 	unsigned long long	value;
@@ -27,7 +27,7 @@ unsigned long long	ft_atoull(char *str, int len, int *match)
 	return (value);
 }
 
-unsigned long long	ft_atoull_base(char *nbr, char *base_from)
+static unsigned long long	ft_atoull_base(char *nbr, char *base_from)
 {
 	int					i;
 	int					len;
@@ -48,7 +48,7 @@ unsigned long long	ft_atoull_base(char *nbr, char *base_from)
 	return (base_nbr);
 }
 
-int					res_len(unsigned long long nbr, char *base_to)
+static int					res_len(unsigned long long nbr, char *base_to)
 {
 	int	base_len;
 	int	res_len;
@@ -63,7 +63,8 @@ int					res_len(unsigned long long nbr, char *base_to)
 	return (res_len);
 }
 
-char				*ft_putnbr_base(unsigned long long nbr, int res_len, char *result, char *base_to)
+static char					*ft_putnbr_base(unsigned long long nbr,
+		int res_len, char *result, char *base_to)
 {
 	int	base_len;
 
@@ -76,7 +77,8 @@ char				*ft_putnbr_base(unsigned long long nbr, int res_len, char *result, char 
 	return (result);
 }
 
-char				*ft_convert_base(char *nbr, char *base_from, char *base_to)
+char						*ft_convert_base(char *nbr,
+		char *base_from, char *base_to)
 {
 	int					len;
 	unsigned long long	base_nbr;
