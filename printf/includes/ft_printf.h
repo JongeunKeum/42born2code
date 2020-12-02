4361d968a06fae271bb7227e151dfceab54315c8
jkeum <jon9eun@gmail.com>
2020-12-02 22:02:46 +0900

    ft_printf.h modified (%f)

--- printf/includes/ft_printf.h
+++ printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 15:38:45 by jkeum             #+#    #+#             */
-/*   Updated: 2020/11/29 17:04:27 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/12/02 16:47:09 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@
 # include <stdlib.h>
 # include <limits.h>
 # include "../libft/libft.h"
+# define BIAS 1023
+
+typedef struct		s_fobj
+{
+	char	deci_bin[1074];
+	int		expnt;
+}					t_fobj;
 
 typedef struct		s_obj
 {
@@ -36,7 +43,19 @@ typedef struct		s_obj
 	int		length;
 	int		return_value;
 	char	*res;
+	t_fobj	fobj;
 }					t_obj;
+
+typedef union		u_fpoint
+{
+	struct			s_bitfield
+	{
+		unsigned long	mantissa : 52;
+		unsigned long	exponent : 11;
+		unsigned long	sign : 1;
+	}				bitfield;
+	double			realnum;
+}					t_fpoint;
 
 int					ft_printf(const char *str, ...);
 int					print_int(va_list args, t_obj *obj);
@@ -48,6 +67,7 @@ int					print_oct(va_list args, t_obj *obj);
 int					print_address(va_list args, t_obj *obj);
 int					print_percent(t_obj *obj);
 int					print_num_ch(va_list args, t_obj *obj);
+int					print_double(va_list args, t_obj *obj);
 int					check_format(const char *str, va_list args, t_obj *obj);
 void				check_length(const char *str, t_obj *obj);
 int					fill_precision_nbr(t_obj *obj);
