9dd687a62d3db45cf3c9078a15af115368fa480c
jkeum <jon9eun@gmail.com>
2020-12-03 22:06:16 +0900

    main_f.c modified

--- printf/srcs/main_f.c
+++ printf/srcs/main_f.c
@@ -3,15 +3,22 @@
 
 int	main(void)
 {
-	printf("01. hello %f world\n", 2.625);
+/*	printf("01. hello %f world\n", 2.625);
 	ft_printf("01. hello %f world\n", 2.625);
 	ft_printf("01. hello %f world\n", 0.625);
+	printf("\n");
 
 	ft_printf("02. hello %f world\n", 5.125);
 	ft_printf("02. hello %f world\n", 0.125);
-
-	ft_printf("03. hello %f world\n", 23.456);
+	printf("\n");
+*/
+	ft_printf("03. hello %f world\n", __DBL_MIN__);
 	ft_printf("03. hello %f world\n", 0.456);
-	
+	printf("\n");
+/*
+	ft_printf("04. hello %f world\n", 12.3125);
+	ft_printf("04. hello %f world\n", 0.3125);
+	printf("\n");
+*/	
 	return (0);
 }
