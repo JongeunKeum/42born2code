1673aaf120f636114e156a07eb6433e0bbdc311e
jkeum <jon9eun@gmail.com>
2020-12-04 23:25:02 +0900

    ft_print_f.c modified (inte_bin/two completed, inte_res not yet)

--- printf/srcs/ft_print_f.c
+++ printf/srcs/ft_print_f.c
@@ -6,7 +6,7 @@
 /*   By: jkeum <jkeum@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 16:40:30 by jkeum             #+#    #+#             */
-/*   Updated: 2020/12/03 21:57:44 by jkeum            ###   ########.fr       */
+/*   Updated: 2020/12/04 23:19:47 by jkeum            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,82 +26,23 @@ static t_fpoint	*init_fobjs(void)
 	return (fp);
 }
 
-static void		process_deci_bin(t_obj *obj, t_fpoint *fp)
-{
-	int				i;
-	int				j;
-	unsigned long	temp;
-
-	if (obj->fobj.expnt >= 0)
-	{
-		temp = fp->bitfield.mantissa << obj->fobj.expnt;
-		i = -1;
-		while (++i < 52)
-		{
-			if ((temp >> (51 - i)) & 1)
-				obj->fobj.deci_bin[i] = 1;
-		}
-	}
-	else
-	{
-		i = obj->fobj.expnt * (-1) - 1;
-		obj->fobj.deci_bin[i] = 1;
-		j = obj->fobj.expnt * -1;
-		while (++i < 52 + j)
-		{
-			if ((fp->bitfield.mantissa >> (51 - i + j)) & 1)
-				obj->fobj.deci_bin[i] = 1;
-		}
-	}
-}
-
-static void		process_deci_five(t_obj *obj, int idx)
-{
-	int	rounding;
-	int	i;
-
-	rounding = 0;
-	i = idx + 1;
-	while (--i > 0)
-	{
-		obj->fobj.deci_five[i] = obj->fobj.deci_five[i - 1] * 5 % 10 + rounding;
-		rounding = obj->fobj.deci_five[i - 1] * 5 / 10;
-	}
-	obj->fobj.deci_five[0] = rounding;
-}
-
-static void		process_deci_res(t_obj *obj, int idx)
-{
-	int	i;
-
-	i = idx + 1;
-	while (--i >= 0)
-	{
-		obj->fobj.deci_res[i] += (obj->fobj.deci_five[i] + obj->fobj.rounding);
-		if (obj->fobj.deci_res[i] >= 10)
-		{
-			obj->fobj.rounding = 1;
-			obj->fobj.deci_res[i] %= 10;
-		}
-		else
-			obj->fobj.rounding = 0;
-	}
-}
-
 int	print_double(va_list args, t_obj *obj)
 {
 	t_fpoint		*fp;
 	int				i;
+	int				j;
 
 	if (!(fp = init_fobjs()))
 		return (0);
 	fp->realnum = va_arg(args, double);
 	obj->fobj.expnt = fp->bitfield.exponent - BIAS;
+
+	// 소수부분
 	process_deci_bin(obj, fp);
-	for (int j = 0; j < 1074; j++)
+/*	for (int j = 0; j < 1074; j++)
 		ft_putnbr_fd(obj->fobj.deci_bin[j], 1);
 	write(1, "\n\n", 2);
-	i = 0;
+*/	i = 0;
 	obj->fobj.deci_five[0] = 5;
 	while (i < 52 - obj->fobj.expnt)
 	{
@@ -117,7 +58,31 @@ int	print_double(va_list args, t_obj *obj)
 			write(1, "\n", 1);
 	*/	process_deci_five(obj, ++i);
 	}
-	for (int i = 0; i < 1074; i++)
-		ft_putnbr_fd(obj->fobj.deci_res[i], 1);
+//	for (int i = 0; i < 1074; i++)
+//		ft_putnbr_fd(obj->fobj.deci_res[i], 1);
+
+	// 정수부분
+	obj->fobj.rounding = 0;
+	process_inte_bin(obj, fp);
+	obj->fobj.inte_two[0] = 1;
+	i = -1;
+	j = 1024;
+	while (++i <= obj->fobj.expnt && j >= 0)
+	{
+		if (obj->fobj.inte_bin[--j] == 1)
+		{
+			process_inte_res(obj);
+			for (int k = 0; k < 309; k++)
+				ft_putnbr_fd(obj->fobj.inte_res[k], 1);
+			write(1, "\n", 1);
+		}
+		process_inte_two(obj);
+/*		for (int j = 0; j < 52; j++)
+			ft_putnbr_fd(obj->fobj.inte_two[j], 1);
+		write(1, "\n", 1);
+*/	}
+	ft_strrev_f(obj);
+//	for (int j = 0; j < 309; j++)
+//		ft_putnbr_fd(obj->fobj.inte_res[j], 1);
 	return (1);
 }
