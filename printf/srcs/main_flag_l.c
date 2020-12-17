8fb0decb380f3197bff9d1d7da8925a436fc42ad
jkeum <jon9eun@gmail.com>
2020-12-17 18:22:55 +0900

    check running time

--- printf/srcs/main_flag_l.c
+++ printf/srcs/main_flag_l.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include "../includes/ft_printf.h"
-
-int	ft_printf(const char *str, ...);
+#include <time.h>
 
 int	main(void)
 {
+	time_t start, end;
+	double result;
+
+	start = time(NULL);
+
 	printf("10:\t\t%d\n", 10);
 	ft_printf("10:\t\t%d\n\n", 10);
 
@@ -22,6 +26,10 @@ int	main(void)
 
 	printf("LLONG_MAX(ll):%lld\n", LLONG_MAX);
 	ft_printf("LLONG_MAX(ll):%lld\n\n", LLONG_MAX);
+
+	end = time(NULL);
+	result = (double)(end - start);
+	printf("%f", result);
 
 	return (0);
 }
