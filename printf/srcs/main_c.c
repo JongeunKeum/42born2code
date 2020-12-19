4ffe6d513037627eb233d012376b9ef5b54ec22e
jkeum <jon9eun@gmail.com>
2020-12-19 18:59:10 +0900

    main_c.c modified (check # flag)

--- printf/srcs/main_c.c
+++ printf/srcs/main_c.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include "../includes/ft_printf.h"
 
-int	ft_printf(const char *str, ...);
-
 int	main(void)
 {
-	printf("01. hell%c %corld\n", 'o', 'W');
-	ft_printf("01. hell%c %corld\n\n", 'o', 'W');
+	// printf("01. hell%c %corld\n", 'o', 'W');
+	// ft_printf("01. hell%c %corld\n\n", 'o', 'W');
 
-	printf("02. hell%5c\n", 'o');
-	ft_printf("02. hell%5c\n\n", 'o');
+	// printf("02. hell%5c\n", 'o');
+	// ft_printf("02. hell%5c\n\n", 'o');
 
-	printf("%d\n", printf("03. hell%c|\n", '\0'));
-	printf("%d\n", ft_printf("03. hell%c|\n\n", '\0'));
+	// printf("%d\n", printf("03. hell%c|\n", '\0'));
+	// printf("%d\n", ft_printf("03. hell%c|\n\n", '\0'));
 
-	printf("04. hell%-5c|\n", '\0');
-	ft_printf("04. hell%-5c|\n\n", '\0');
+	// printf("04. hell%-5c|\n", '\0');
+	// ft_printf("04. hell%-5c|\n\n", '\0');
+
+	ft_printf("05. hell%#5c\n", 'o');
 
 	return (0);
 }
